c0574ddd6e32d1da6a9831838cfa99d4ae6423b2
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2023-08-19 11:31:22 +0200

    L20 Converted the string value to a boolean

--- scripts.js
+++ scripts.js
@@ -1,34 +1,85 @@
-const root1 = document.querySelector(['data-key="order1"']);
-console.log (root)
-const biscuits =document.querySelector(['data-biscuits="10"']);
-console.log (biscuits)
-const 1-donuts: document(donuts),
-const 1-pancakes: document(pancakes),
-const 1-status: document(status)
+const root1 = document.querySelector('[data-key="order1"]');
+/**
+ * the element with the attribute data-biscuits="10"
+ */
+let biscuits1 = document.querySelector('[data-biscuits="10"]');
+/**
+ * the value of the attribute  [data-biscuits]  && converted to a number
+ */
+let biscuits1value = parseInt(biscuits1.getAttribute("data-biscuits")); 
+console.log(biscuits1value);
 
-const 2 = document(order2),
-const 2-biscuits: document(biscuits),
-const 2-donuts: document(donuts),
-const 2-pancakes: document(pancakes),
-const 2-status: document(status)
+let donuts1 = document.querySelector('[  data-donuts="13" ]');
+let donuts1Value = parseInt(donuts1.getAttribute("data-donuts"));
+console.log(donuts1Value);
 
-const 3-root = document(order3),
-const 3-biscuits: document(biscuits),
-const 3-donuts: document(donuts),
-const 3-pancakes: document(pancakes),
-const 3-status: document(status)
+let pancakes1 = document.querySelector('[data-pancakes="0"]');
+const pancakes1Value = parseInt(pancakes1.getAttribute("data-pancakes"));
 
-1-biscuits= 1-root.biscuits,
-1-donuts = 1-root.donuts,
-1-pancakes = 1-root.pancakes,
-1-status = 1-root.status ? Delivered : Pending
+let status1 = document.querySelector("[data-delivered]");
+const status1value = status1.getAttribute("data-delivered")
+/**
+ * converts [data-delivery] value to a boolean
+ */
+const status1Boolean = status1value==="true";   
+console.log (status1Boolean)
 
-2-biscuits= 2-root.biscuits,
-2-donuts = 2-root.donuts,
-2-pancakes = 2-root.pancakes,
-2-status = 2-root.status ? Delivered : Pending
+  if  (status1Boolean ===false){  console.log (false);
+  } else {console.log (true)};
+  
 
-3-biscuits= 3-root.biscuits,
-3-donuts = 3-root.donuts,
-3-pancakes = 3-root.pancakes,
-3-status = 3-root.status ? Delivered : Pending
+console.log (status1va)
+let root2 = document.querySelector('[data-key="order2"]');
+
+const biscuits2 = document.querySelector('[data-biscuits="5"]');
+const biscuits2Value = parseInt(biscuits2.getAttribute("data-biscuits"));
+console.log (biscuits2Value)
+
+const donuts2 = document.querySelector('[data-donuts="0"]');
+const donuts2Value = parseInt(donuts2.getAttribute("data-donuts"));
+
+const pancakes2 = document.querySelector('[data-pancakes="2"]');
+const pancakes2Value = parseInt(pancakes2.getAttribute("data-pancakes"));
+
+const status2 = document.querySelector(".status");
+
+const root3 = document.querySelector('[data-key="order3"]');
+
+const biscuits3 = document.querySelector('[data-biscuits="12"]');
+const biscuits3Value = parseInt(biscuits3.getAttribute("data-biscuits"));
+
+const donuts3 = document.querySelector('[data-donuts="11"]');
+const donuts3Value = parseInt(donuts3.getAttribute('data-donuts'));
+console.log(donuts3Value);
+
+const pancakes3 = document.querySelector('[data-pancakes="15"]'); 
+const pancakes3Value=parseInt( pancakes3.getAttribute ('data-pancakes'))
+console.log (pancakes3Value)
+
+const status3 = document.querySelector(".status");
+/**element [biscuits] with a child element [count] */
+let biscuits1Count = document.querySelector ('.biscuits .count');
+ biscuits1Count.textContent = biscuits1value;
+console.log (biscuits1Count);
+
+ let donuts1Count = document.querySelector ('.donuts .count');
+  donuts1Count.textContent= donuts1Value
+console.log(donuts1Count);
+
+ let pancakes1Count = document.querySelector ('.pancakes  .count')
+ pancakes1Count.textContent= pancakes1Value
+console.log (pancakes1Count)
+
+  status1 = root1.status ? Delivered : Pending;
+
+let biscuits2Count = document.querySelector('.biscuits  .count')
+biscuits2Count.textContent = biscuits2Value
+
+  donuts3 = root2.donuts
+  pancakes3 = root2.pancakes
+  status3 = root2.status ? Delivered : Pending;
+
+(biscuits3 = root3.biscuits),
+  (donuts3 = root3.donuts),
+  (pancakes3 = root3.pancakes),
+  (status3 = root3.status ? Delivered : Pending);
